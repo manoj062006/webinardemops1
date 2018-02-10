2f20a714a39be34f64117a5627442bfaf9930801
manoj062006 <manoj062006@hotmail.com>
2018-02-10 22:31:54 +0530

    Add project files.

---
+++ webinardemops1/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webinardemops1
+{
+    class Employee
+    {
+        private int _empID;
+        private string _empName;
+
+        public int EmpID { get { return _empID; } }
+        public string EmpName { get { return _empName; } }
+
+
+        public Employee()
+        {
+
+        }
+        public Employee(int empId, string EmpName)
+        {
+            this._empID = empId;
+            this._empName = EmpName;
+
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("EmployeeID is " + EmpID);
+            Console.WriteLine("EmployeeName is " + EmpName);
+        }
+    }
+}
---
+++ webinardemops1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace webinardemops1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hello World!");
+
+            Employee e = new Employee(101, "kedar");
+            e.DisplayEmployeeDetails();
+            Console.ReadLine();
+        }
+    }
+}
